826d2b3b736712f43c767529851abd17423318f9
Navido Mangumo <nmangumo@gmail.com>
2020-12-28 22:33:55 +0200

    Produto e associacao muitos para muitos

--- src/main/java/com/navidomangumo/cursomc/domain/Categoria.java
+++ src/main/java/com/navidomangumo/cursomc/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.navidomangumo.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable{
@@ -16,6 +19,8 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer Id;
 	private String nome;
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
 		
@@ -43,6 +48,14 @@ public class Categoria implements Serializable{
 		this.nome = nome;
 	}
 
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -67,6 +80,8 @@ public class Categoria implements Serializable{
 			return false;
 		return true;
 	}
+
+	
 	
 	
 
