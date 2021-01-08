faf470d4a4b1ab6f8a49c1bf1599e3e615516dcb
Navido Mangumo <nmangumo@gmail.com>
2021-01-08 22:31:34 +0200

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/navidomangumo/cursomc/CursomcApplication.java
+++ src/main/java/com/navidomangumo/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.navidomangumo.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.navidomangumo.cursomc.domain.Cidade;
 import com.navidomangumo.cursomc.domain.Cliente;
 import com.navidomangumo.cursomc.domain.Endereco;
 import com.navidomangumo.cursomc.domain.Estado;
+import com.navidomangumo.cursomc.domain.Pagamento;
+import com.navidomangumo.cursomc.domain.PagamentoComBoleto;
+import com.navidomangumo.cursomc.domain.PagamentoComCartao;
+import com.navidomangumo.cursomc.domain.Pedido;
 import com.navidomangumo.cursomc.domain.Produto;
+import com.navidomangumo.cursomc.domain.enums.EstadoPagamento;
 import com.navidomangumo.cursomc.domain.enums.TipoCliente;
 import com.navidomangumo.cursomc.repositories.CategoriaRepository;
 import com.navidomangumo.cursomc.repositories.CidadeRepository;
 import com.navidomangumo.cursomc.repositories.ClienteRepository;
 import com.navidomangumo.cursomc.repositories.EnderecoRepository;
 import com.navidomangumo.cursomc.repositories.EstadoRepository;
+import com.navidomangumo.cursomc.repositories.PagamentoRepository;
+import com.navidomangumo.cursomc.repositories.PedidoRepository;
 import com.navidomangumo.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -35,6 +43,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -82,6 +94,22 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 10:32"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUIDATO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
+		
 	}
 
 }
--- src/main/java/com/navidomangumo/cursomc/domain/Cliente.java
+++ src/main/java/com/navidomangumo/cursomc/domain/Cliente.java
@@ -37,6 +37,9 @@ public class Cliente implements Serializable{
 	@CollectionTable(name="telefone")
 	private Set<String> telefone = new HashSet<>();
 	
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente () {
 		
 	}
@@ -105,6 +108,14 @@ public class Cliente implements Serializable{
 	public void setTelefone(Set<String> telefone) {
 		this.telefone = telefone;
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -130,6 +141,8 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
+
+	
 	
 	
 	
