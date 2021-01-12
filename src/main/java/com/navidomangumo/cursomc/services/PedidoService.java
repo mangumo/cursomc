eccf9f723d8284249fc3954239e15e1578dc6e68
Navido Mangumo <nmangumo@gmail.com>
2021-01-12 22:33:30 +0200

    Endpoint /pedidos{id} disponibilizado

---
+++ src/main/java/com/navidomangumo/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.navidomangumo.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.navidomangumo.cursomc.domain.Pedido;
+import com.navidomangumo.cursomc.repositories.PedidoRepository;
+import com.navidomangumo.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer Id) {		
+		Optional<Pedido> obj = repo.findById(Id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException(
+				"Objecto nao encontrado! Id: "+ Id +", Tipo: "+ Pedido.class.getName()));
+	}
+
+}
