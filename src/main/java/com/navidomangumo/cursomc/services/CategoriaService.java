ba0bfcb05ab5234043070dd8accf27610317b1ec
mangumo <nmangumo@gmail.com>
2021-04-28 12:12:02 +0200

    Correccoes Update em CategoriaService e construtor de Pagamento

--- src/main/java/com/navidomangumo/cursomc/services/CategoriaService.java
+++ src/main/java/com/navidomangumo/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.navidomangumo.cursomc.domain.Categoria;
+import com.navidomangumo.cursomc.domain.Cliente;
 import com.navidomangumo.cursomc.dto.CategoriaDTO;
 import com.navidomangumo.cursomc.repositories.CategoriaRepository;
 import com.navidomangumo.cursomc.services.exceptions.DataIntegrityException;
@@ -34,10 +35,10 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj,obj);
+		return repo.save(newObj);
 	}
-	
 	public void delete(Integer id) {
 		find(id);
 		try {
@@ -60,5 +61,10 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDTO) {
 		return new Categoria(objDTO.getId(), objDTO.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+		
 	}
 }
